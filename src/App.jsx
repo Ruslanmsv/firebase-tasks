c5b748327c880eb7d7f28a3cdffe6780af5b4333
Ruslan Musaev <ruslan.msv@gmail.com>
2022-09-26 09:22:29 +0200

    Fix with refs (in educational purpose only)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import React from 'react';
-
-import { useState } from "react";
+import { useState, useRef } from "react";
 import AddTask from "./components/AddTask/AddTask";
 import TaskList from "./components/TaskList/TaskList";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  const [taskName, setTaskName] = useState("");
-
-  const taskNameInputRef = React.createRef();
+  const taskNameInputRef = useRef(null);
 
   const addTaskHandler = (event) => {
     event.preventDefault();
-    console.log('in add task handler');
-    console.log('adding to task list: ' + taskName);
-    // setTaskName('');
-    taskNameInputRef.current.value = '';
-    setTasks((prevTasks) => prevTasks.concat(taskName));
+    const task = taskNameInputRef.current.value;
+    taskNameInputRef.current.value = "";
+    setTasks((prevTasks) => prevTasks.concat(task));
   };
 
-  const inputChangeHandler = (event) => {
-    console.log('in input change handler');
-    console.log(event.target.value);
-    setTaskName(event.target.value);
-  };
   return (
     <>
-      <AddTask ref={taskNameInputRef} taskName={taskName} inputChangeHandler={inputChangeHandler} addTaskHandler={addTaskHandler}/>
+      <AddTask
+        ref={taskNameInputRef}
+        addTaskHandler={addTaskHandler}
+      />
       <TaskList tasks={tasks} />
     </>
   );
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import s from './AddTask.module.css';
-const AddTask = (props) => {
+import { forwardRef, useImperativeHandle } from "react";
+import s from "./AddTask.module.css";
+const AddTask = forwardRef((props, ref) => {
   return (
     <form className={s["add-task"]}>
       <label htmlFor="task-input">Task name</label>
-      <input ref={props.tasnNameInputRef} type="text" id="task-input" value={props.taskName} onChange={props.inputChangeHandler}></input>
-      <button onClick={props.addTaskHandler} type="submit">Add Task</button>
+      <input
+        ref={ref}
+        type="text"
+        id="task-input"
+        // value={props.taskName}
+        // onChange={props.inputChangeHandler}
+      ></input>
+      <button onClick={props.addTaskHandler} type="submit">
+        Add Task
+      </button>
     </form>
   );
-};
+});
 
 export default AddTask;
